3fc3ad559294b76d85efc1ff04458041134ff0fb
saakaarb <saakaar28@gmail.com>
2024-12-04 19:29:54 -0800

    code organization

--- Front_end.py
+++ Front_end.py
@@ -1,29 +1,25 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import os
 from os import sys, path
-sys.path.append(r'/home/saakaar/Desktop/Neural Networks/All_data')
-
-
-# In[2]:
-
-
 import back_end
 
 
-# In[3]:
+if __name__=='__main__':
+
+    
+    num_conv_layers=3
+    num_dense_layers=1
+    reg_weight= 1E-5
+    num_datapts= 252
+    num_training_iters=25
+    val_split=0.15
+    inp_activation='swish'
+
+    optim_name='adam'
+    loss_type='mse'
+
+    model=back_end.build_model(num_conv_layers,num_dense_layers,optim_name,loss_type,reg_weight,inp_activation)
 
 
-model=back_end.build_model(3,1,'adam','mse')
-
-
-# In[4]:
-
-
-model=back_end.train_model(252,25000,0.15,model)
+    model=back_end.train_model(num_datapts,num_training_iters,val_split,model)
 
 
